33f941764987f3eb6e613ce0f006b2ad559eedd9
Miso Tanaka <miso.develop@gmail.com>
2019-09-12 00:49:42 +0900

    create!

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+const { execSync } = require('child_process')
+
+// TODO: 引数チェックいれて
+const subDomain = process.argv[2] || "xxxxxxxx"
+const port = process.argv[3] || 3000
+
+console.log(`https://${subDomain}.serveo.net/`)
+
+while (true) {
+	try {
+		// TODO: サブドメイン埋まってたときの
+		execSync(`ssh -R ${subDomain}:80:localhost:${port} serveo.net`)
+	} catch {}
+}
